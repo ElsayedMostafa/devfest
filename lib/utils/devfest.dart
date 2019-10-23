3599e812956d023d7bfb28b5dbf4e43746607ec1
ElsayedMostafa <elsayedmostafa03@gmail.com>
2019-10-23 16:13:07 +0200

    first commit

---
+++ lib/utils/devfest.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DevFest{
+  static const String app_name = "Devfest";
+  static const String app_version = "Version 1.0.4";
+  static const int app_version_code = 1;
+  static const String app_color = "#ffd7167";
+  static Color primaryAppColor = Colors.white;
+  static Color secondaryAppColor = Colors.black;
+  static const String google_sans_family = "GoogleSans";
+  static bool isDebugMode = true;
+
+  //* Preferences
+  static SharedPreferences prefs;
+  static const String loggedInPref = "loggedInPref1";
+  static const String displayNamePref = "displayNamePref";
+  static const String emailPref = "emailPref";
+  static const String phonePref = "phonePref";
+  static const String photoPref = "photoPref";
+  static const String isAdminPref = "isAdminPref";
+  static const String darkModePref = "darkModePref";
+}
